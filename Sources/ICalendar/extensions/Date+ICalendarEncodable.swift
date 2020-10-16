fd5183a3b6d783741050851bda62ce58f34278c7
fwcd <fwcdmail@gmail.com>
2020-10-16 22:38:30 +0200

    Always use UTC

--- Sources/ICalendar/extensions/Date+ICalendarEncodable.swift
+++ Sources/ICalendar/extensions/Date+ICalendarEncodable.swift
@@ -3,6 +3,7 @@ import Foundation
 extension Date: ICalendarEncodable {
     public var iCalendarEncoded: String {
         let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.dateFormat = "yyyyMMdd'T'HHmmss"
         return formatter.string(from: self)
     }
